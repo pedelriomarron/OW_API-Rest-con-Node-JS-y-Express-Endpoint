e4501fa51072947e2e188767e5c3b00d7bd03782
pedelriomarron <pedelriomarron@gmail.com>
2022-03-27 00:01:06 +0100

    Añadir nodemon

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hola mundo")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/hello',(req,res)=>{
+    res.status(200).send({"status":"OKi"})
+})
+
+app.listen(port,()=>{
+    console.log("Servidor del API REST esta funcionando...")
+})
